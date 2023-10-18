69ced643df4d1aab296fa7eb8e765d42285f3403
Eliseo Canaviri <53970655+Eliseo-Canaviri@users.noreply.github.com>
2023-10-18 14:54:32 -0400

    Eliseo
    index.js

---
+++ Assets/js/index.js
@@ -0,0 +1,18 @@
+// Obtén referencias a los enlaces de navegación
+const enlaceAlarmas = document.querySelector('a[href="#mencion"]');
+// Agrega manejadores de eventos a los enlaces
+enlaceMencion.addEventListener('click', scrollToSection);
+
+// Función para desplazarse suavemente a la sección correspondiente
+function scrollToSection(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href').substring(1); // Obtén el ID de la sección
+    const targetSection = document.getElementById(targetId);
+    const offset = 70; // Ajusta el desplazamiento vertical
+
+    // Realiza el desplazamiento suave
+    window.scrollTo({
+        top: targetSection.offsetTop - offset,
+        behavior: 'smooth'
+    });
+}
